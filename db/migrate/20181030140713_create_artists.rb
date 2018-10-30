e97fa959d7b9bc9a68eb53f54946585801b9c47e
Eric Fiorani <efiorani305@gmail.com>
2018-10-30 14:15:02 +0000

    automatically backed up by learn

--- db/migrate/20181030140713_create_artists.rb
+++ db/migrate/20181030140713_create_artists.rb
@@ -1,6 +1,6 @@
-class CreateArtists < ActiveRecord::Migration
+class CreateArtist < ActiveRecord::Migration
   def change
-    create_table :artists do |t|
+    create_table :artist do |t|
       t.string :name
       t.text :bio
 
