d7ba6aa063d256c42822d85ee73950feff4d0741
Eric Fiorani <efiorani305@gmail.com>
2018-10-30 14:30:32 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,32 @@
 class ArtistsController < ApplicationController
+
+  def show
+    @artist = Genre.find(params[:id])
+  end
+
+  def new
+    @artist = Genre.new
+  end
+
+  def create
+    @artist = Genre.new(artist_params(:name))
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+    @artist = Genre.find(params[:id])
+  end
+
+  def update
+    @artist = Genre.find(params[:id])
+    @artist.update(artist_params(:name))
+    redirect_to artist_path(@artist
+  end
+
+  private
+
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
+  end
 end
