495ac91dd775fd2650badb2b262cfe07e5b0851c
Eric Fiorani <efiorani305@gmail.com>
2018-10-30 14:29:12 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -23,4 +23,9 @@ class GenresController < ApplicationController
     @genre.update(genre_params(:name))
     redirect_to genre_path(@genre
   end
+
+  private
+
+  def genre_params(*args)
+    params.require(:genre).permit(:name)
 end
