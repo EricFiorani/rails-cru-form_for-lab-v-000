708825c146a8d11739d4009561be2ee4b32e6fee
Eric Fiorani <efiorani305@gmail.com>
2018-10-30 14:15:58 +0000

    automatically backed up by learn

--- db/migrate/20181030140831_create_songs.rb
+++ db/migrate/20181030140831_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :name
-      t.artist_id :integer
+      t.integer :artist_id
       t.genre_id :integer
 
       t.timestamps null: false
