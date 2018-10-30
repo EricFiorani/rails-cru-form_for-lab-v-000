b74a58d66b27de045f899e4594981ce9021f5ece
Eric Fiorani <efiorani305@gmail.com>
2018-10-30 14:53:18 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,7 +24,7 @@ class SongsController < ApplicationController
 
   def update
     @song = Song.find(params[:id])
-    @song.update(song_params(:name, :artists_id, :genre_id))
+    @song.update(song_params(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
   end
 
