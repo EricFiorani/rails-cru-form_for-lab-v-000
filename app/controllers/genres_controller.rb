9ae3219a5c6672982c72211d070311bc798c1604
Eric Fiorani <efiorani305@gmail.com>
2018-10-30 14:25:57 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,4 +3,16 @@ class GenresController < ApplicationController
   def show
     @genre = Genre.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
